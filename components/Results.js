95f7b74481fcaeed5461c45a75a372c1e5e1b40e
Sahandgha <sahand.gha@gmail.com>
2021-12-29 09:07:40 +1000

    style thumbnail component and make the result section responsive

--- components/Results.js
+++ components/Results.js
@@ -2,7 +2,7 @@ import Thumbnail from "./Thumbnail";
 
 export default function Results({ results }) {
   return (
-    <div>
+    <div className="bg-gray-700 text-gray-200 sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,18 +4,21 @@ import Image from "next/image";
 export default function Thumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-    <div>
+    <div className="p-3 cursor-pointer hover:text-white active:text-red-400 select-none xl:hover:scale-105 transition-transform duration-200">
       <Image
         src={`${BASE_URL}${result.backdrop_path}`}
         width="200"
         height="100"
         layout="responsive"
       />
-      <div className="">
-        <p>{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
-        <p>
-          {result.release_date || result.first_air_date} - <ThumbUpIcon />
+      <div className="p-2">
+        <p className="truncate">{result.overview}</p>
+        <h2 className="text-xl font-bold">
+          {result.title || result.original_name}
+        </h2>
+        <p className="flex items-center">
+          {result.release_date || result.first_air_date}{" "}
+          <ThumbUpIcon className="h-5 mx-2" />
           {result.vote_count}
         </p>
       </div>
