3806627e29fc890880c5863e0deb0ce99fdfb158
Sahandgha <sahand.gha@gmail.com>
2021-12-28 12:12:16 +1000

    create the navbar and utils section

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,8 +1,8 @@
 export default function HeaderIcon({ title, Icon }) {
   return (
-    <div className="m-nw-20 flex flex-col justify-center items-center select-none cursor-pointer hover:text-white active:text-red-400 sm:w-50">
+    <div className="m-3 w-20 flex flex-col justify-center items-center select-none cursor-pointer hover:text-white active:text-red-400 sm:w-50">
       <Icon className="h-8" />
-      <p>{title}</p>
+      <p className="my-2">{title}</p>
     </div>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,17 @@
+import requests from "../utils/requests";
+import { useRouter } from "next/router";
 export default function Navbar() {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
+  const router = useRouter();
+  return (
+    <div className="flex bg-zinc-500 text-gray-200 justify-center p-8 text-xl  sm:text-2xl">
+      {Object.entries(requests).map(([key, { title, url }]) => (
+        <p
+          onClick={() => router.push(`?genre=${key}`)}
+          className="mx-8 select-none cursor-pointer hover:text-white active:text-red-400"
+        >
+          {title}
+        </p>
+      ))}
+    </div>
+  );
 }
