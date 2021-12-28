77eb79528239b2b431ac4d7a95cda4f3872e5b78
Sahandgha <sahand.gha@gmail.com>
2021-12-29 09:12:20 +1000

    add key to Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,6 +6,7 @@ export default function Navbar() {
     <div className="flex bg-zinc-500 text-gray-200 justify-center p-8 text-xl  sm:text-2xl">
       {Object.entries(requests).map(([key, { title, url }]) => (
         <p
+          key={key}
           onClick={() => router.push(`?genre=${key}`)}
           className="mx-8 select-none cursor-pointer hover:text-white active:text-red-400"
         >
