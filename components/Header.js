2c93b67d28ec958b87c172055c81345d2c0eb647
Sahandgha <sahand.gha@gmail.com>
2021-12-28 11:37:36 +1000

    style the header section

--- components/Header.js
+++ components/Header.js
@@ -9,17 +9,18 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div>
-      <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col p-8 sm:flex-row justify-between">
+      <div className="mb-4 flex justify-center items-center">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
         <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
       </div>
       <Image
+        className="cursor-pointer active:brightness-110"
         src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
-        width={200}
-        height={100}
+        width={100}
+        height={50}
       />
     </div>
   );
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,6 +1,6 @@
 export default function HeaderIcon({ title, Icon }) {
   return (
-    <div>
+    <div className="m-nw-20 flex flex-col justify-center items-center select-none cursor-pointer hover:text-white active:text-red-400 sm:w-50">
       <Icon className="h-8" />
       <p>{title}</p>
     </div>
